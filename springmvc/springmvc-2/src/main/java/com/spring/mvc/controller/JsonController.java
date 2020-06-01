8dab788590db8e61d87d064519c0d433feb98bb8
nick <sjliwei2002@126.com>
2020-06-01 16:51:00 +0800

    JSON controller完善：增加x-www-form-urlencoded、form-data和raw json提交给controller，转换为pojo对象，然后通过@ResponseBody返回pojo对象

--- springmvc/springmvc-2/src/main/java/com/spring/mvc/controller/JsonController.java
+++ springmvc/springmvc-2/src/main/java/com/spring/mvc/controller/JsonController.java
@@ -3,6 +3,7 @@ package com.spring.mvc.controller;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,12 +21,35 @@ import com.spring.mvc.integrate.User;
 public class JsonController {
 
 	@ResponseBody
-	@RequestMapping("/user")
-	public User get() {
+	@RequestMapping("/user1")
+	public User get1() {
 		User user = new User();
 		user.setId(1);
 		user.setName("jayjay");
 		user.setBirth(new Date());
+		return user;
+	}
+	//input: 	json NOK
+	//			x-www-for-urlencoded OK
+	//			form-data: OK
+	//output:json
+	@ResponseBody
+	@RequestMapping("/user2")
+	public User get2(User user) {
+		System.out.println(user.getId()+user.getName());
+		user.setId(user.getId()+1);
+		return user;
+	}
+
+	//input: 	json OK
+	//			x-www-for-urlencoded NOK
+	//			form-data: NOK
+	//output:json
+	@ResponseBody
+	@RequestMapping("/user3")
+	public User get3(@RequestBody User user) {
+		System.out.println(user.getId()+user.getName());
+		user.setId(user.getId()+1);
 		return user;
 	}
 }
