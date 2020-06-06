340520b75e5dea17d137a0c1bd611d35730b6eca
nick <sjliwei2002@126.com>
2020-06-06 09:39:15 +0800

    ssm增加springmvc部分

---
+++ ssm/ssm-1/src/main/java/com/hust/controller/messageController.java
@@ -0,0 +1,37 @@
+package com.hust.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class messageController {
+    @RequestMapping("/message/go")
+    public String redirect(){
+        return "reach";
+    }
+
+    @RequestMapping("/message/detail/data={uname}")
+    public String searchUser(@PathVariable("uname")String data, Model model){
+        model.addAttribute("data", data);
+        return "detail";
+    }
+
+    @RequestMapping("/message/report1")
+    public String report1(String begin, String end, Model model){
+        model.addAttribute("begin", begin);
+        model.addAttribute("end", end);
+        return "report1";
+    }
+
+    @RequestMapping("/message/report2")
+    public String report2(@RequestParam("begin") String Nbegin, @RequestParam("end") String Nend, Model model){
+        model.addAttribute("begin", Nbegin);
+        model.addAttribute("end", Nend);
+        return "report1";
+    }
+
+
+}
--- ssm/ssm-1/src/main/webapp/index.jsp
+++ ssm/ssm-1/src/main/webapp/index.jsp
@@ -1,5 +1,20 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="message/go">i am going to redirect</a>
+<div>
+    username:<input type="text" id="userName" name="userName"/>
+    <input type="button" value="find user" onClick="Search()"/>
+    <form id="report" action="/message/report2">
+        <input id="begin" type="text" name="begin"/>
+        <input id="end" type="text" name="end"/>
+        <input id="reportCommit" type="submit" name="submmit"/>
+    </form>
+</div>
 </body>
 </html>
+<script>
+    function Search(){
+        window.open("/message/detail/data="+document.getElementById("userName").value);
+    }
+</script>
