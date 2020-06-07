2dc26b68fe457c976ad540c7d0dc162942940e38
nick <sjliwei2002@126.com>
2020-06-07 12:37:31 +0800

    front and back seperate

---
+++ ssm/ssm-2/src/main/java/com/hust/entity/User.java
@@ -0,0 +1,43 @@
+package com.hust.entity;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String mark;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+}
