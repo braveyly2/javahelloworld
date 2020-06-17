37f985c83485715561c9082bc8f24c3de65d3cb8
nick <sjliwei2002@126.com>
2020-06-17 11:21:31 +0800

    request和response都是json对象，基线版本

--- ssm/ssm-2-projectname/src/main/java/com/hust/controller/UserController.java
+++ ssm/ssm-2-projectname/src/main/java/com/hust/controller/UserController.java
@@ -34,4 +34,63 @@ public class UserController {
         restBean.setObj((Object)user);
         return restBean;
     }
+
+    @RequestMapping(value="/user/register",method=RequestMethod.POST)
+    @ResponseBody
+    public String register(@RequestParam("userName") String name, @RequestParam("password") String password){
+        User user = userService.selectByName(name);
+        //cannot register for the same username
+        if(null != user){
+            return "fail";
+        }
+
+        User userAdd = new User();
+        userAdd.setName(name);
+        userAdd.setPassword(password);
+        userAdd.setMark("thisisforregister");
+
+        userService.insert(userAdd);
+        // model.addAttribute("status","0");
+        return "ok";
+    }
+
+    @RequestMapping(value="/user/register2",method=RequestMethod.POST)
+    @ResponseBody
+    public String register2(@RequestBody User user){
+        User userExist = userService.selectByName(user.getName());
+        //cannot register for the same username
+        if(null != userExist){
+            return "fail";
+        }
+
+        User userAdd = new User();
+        userAdd.setId(user.getId());
+        userAdd.setName(user.getName());
+        userAdd.setPassword(user.getPassword());
+        userAdd.setMark(user.getMark());
+
+        userService.insert(userAdd);
+        // model.addAttribute("status","0");
+        return "ok";
+    }
+
+    @RequestMapping(value="/user/register3",method=RequestMethod.POST)
+    @ResponseBody
+    public RestBean register3(@RequestBody User user){
+        User userExist = userService.selectByName(user.getName());
+        //cannot register for the same username
+        if(null != userExist){
+            return RestBean.error("user:" + user.getName() + " is exist");
+        }
+
+        User userAdd = new User();
+        userAdd.setId(user.getId());
+        userAdd.setName(user.getName());
+        userAdd.setPassword(user.getPassword());
+        userAdd.setMark(user.getMark());
+
+        userService.insert(userAdd);
+        // model.addAttribute("status","0");
+        return RestBean.ok("Succeed to add user",userAdd);
+    }
 }
