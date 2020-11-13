002edaf32d444dc998b8312851fe8524476ee420
nick <sjliwei2002@126.com>
2020-11-13 10:15:01 +0800

    mybatis-7：一对多（user->order）和多对多（student->teacher teacher->student）selectAll OK

--- mybatis/mybatis-7/src/main/java/com/hust/lw/test/MyBatisTest.java
+++ mybatis/mybatis-7/src/main/java/com/hust/lw/test/MyBatisTest.java
@@ -1,8 +1,8 @@
 package com.hust.lw.test;
 
 import com.hust.lw.dao.*;
-import com.hust.lw.model.THusband;
-import com.hust.lw.model.TWife;
+import com.hust.lw.model.*;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -31,6 +31,7 @@ public class MyBatisTest {
     private TUserMapper tUserMapper;
     private TTeacherMapper tTeacherMapper;
     private TStudentMapper tStudentMapper;
+    private TOrderMapper tOrderMapper;
     private InputStream in;
     private SqlSession session;
 
@@ -48,6 +49,10 @@ public class MyBatisTest {
         //5.创建Dao接口的代理对象
         tWifeMapper = session.getMapper(TWifeMapper.class);
         tHusbandMapper = session.getMapper(THusbandMapper.class);
+        tUserMapper = session.getMapper(TUserMapper.class);
+        tOrderMapper = session.getMapper(TOrderMapper.class);
+        tStudentMapper = session.getMapper(TStudentMapper.class);
+        tTeacherMapper = session.getMapper(TTeacherMapper.class);
     }
 
     @After//在测试方法执行完成之后执行
@@ -80,19 +85,58 @@ public class MyBatisTest {
 
     //测试一对多关联
     @Test
-    public void testUserMapper() {
+    public void testUserMapperWrite() {
+        TUser user = new TUser();
+        user.setId(1L);
+        user.setPhone("13638679973");
+        tUserMapper.insert(user);
+
+        TOrder order1 = new TOrder();
+        //order1.setId(1L);
+        order1.setUserId(1L);
+        order1.setNumber(10);
+        tOrderMapper.insert(order1);
+        session.commit();
+
+        TOrder order2 = new TOrder();
+        //order2.setId(2L);
+        order2.setUserId(1L);
+        order2.setNumber(20);
+        tOrderMapper.insert(order2);
+        session.commit();
+
+        TOrder order3 = new TOrder();
+        //order3.setId(3L);
+        order3.setUserId(1L);
+        order3.setNumber(30);
+        tOrderMapper.insert(order3);
+        session.commit();
+
+    }
+
+    @Test
+    public void testUserMapperRead() {
+        TUser user = tUserMapper.selectAll(6L);
+        System.out.println(user.getOrderList());
+    }
+    //测试多对多关联
+    @Test
+    public void testTeacherStudentMapperWrite() {
 
     }
 
     //测试多对多关联
     @Test
-    public void testTeacherMapper() {
-
+    public void testStudentMapperRead() {
+        TStudent tStudent = tStudentMapper.selectAll(1L);
+        System.out.println(tStudent.getTeacherList());
     }
 
     //测试多对多关联
     @Test
-    public void testStudentMapper() {
+    public void testTeacherMapperRead() {
+        TTeacher tTeacher = tTeacherMapper.selectAll(3L);
+        System.out.println(tTeacher.getStudentList());
 
     }
 }
