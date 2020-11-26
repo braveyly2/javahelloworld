ee8a950b25b99473d212d8adf4efa080e2b0c33e
nick <sjliwei2002@126.com>
2020-11-26 10:44:16 +0800

    springmvc-5：转换器和格式化器，自定义转换器

---
+++ springmvc/springmvc-5/ssm-1/src/main/java/com/hust/converter/EmployeeConverter.java
@@ -0,0 +1,24 @@
+package com.hust.converter;
+
+
+import com.hust.entity.Employee;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component("employeeConverter")
+public class EmployeeConverter implements Converter<String, Employee> {
+
+    @Override
+    public Employee convert(String str) {
+        Employee emp=null;
+        //字符串格式 name-age-gender
+        if(str!=null && str.split("-").length==3){
+            emp=new Employee();
+            String[] arr=str.split("-");
+            emp.setName(arr[0]);
+            emp.setAge(Integer.parseInt(arr[1]));
+            emp.setGender(arr[2]);
+        }
+        return emp;
+    }
+}
