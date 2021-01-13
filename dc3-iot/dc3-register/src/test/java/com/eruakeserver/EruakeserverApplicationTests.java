b2ec5c9013472eabffc8cbebe2157f8f8ce87d4e
nick <sjliwei2002@126.com>
2021-01-13 14:16:44 +0800

    init dc3-iot register和auth运行并调通

---
+++ dc3-iot/dc3-register/src/test/java/com/eruakeserver/EruakeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eruakeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class EruakeserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
